ceca0f624aa83a61cbb4d9b9965cd1360711cb29
Alperen Gönül <alperen2009g@gmail.com>
2026-06-28 02:37:24 +0300

    update

--- apps/app.js
+++ apps/app.js
@@ -1,18 +1,59 @@
-text = document.getElementById("aciklama-text");
+// Textler
+let text0 = document.getElementById("aciklama-text");
+let text1 = document.getElementById("text-1");
+let text2 = document.getElementById("text-2");
 
-yazi = ["H", "e", "r"," ","t","ü","r","d","e","n"," ","y","a","z","ı","l","ı","m"];
-discover_button = document.getElementById("discover-button");
-    
+// Yazılacak yazılar
+yazi0 = ["H", "e", "r", " ", "t", "ü", "r", "d", "e", "n", " ", "y", "a", "z", "ı", "l", "ı", "m"];
+yazi1 = ["S", "O", "F", "T", "W", "A", "R", "E"];
+yazi2 = ["G", "İ", "Z", "L", "i", "L", "İ", "K"];
+
+// Butonlar
+let discover_button = document.getElementById("discover-button");
+
 let i = 0;
-text.innerHTML = "";
+let j = 0;
+let k = 0;
 
-const zamanlayici = setInterval(() => {
-    if (i < yazi.length) {
-        text.textContent += yazi[i];
+if (text0) text0.textContent = "";
+if (text1) text1.textContent = "";
+if (text2) text1.textContent = "";
+
+const zamanlayici0 = setInterval(() => {
+    if (i < yazi0.length) {
+        if (text0) text0.textContent += yazi0[i];
         i++;
     } else {
-        clearInterval(zamanlayici);
+        clearInterval(zamanlayici0);
         discover_button.style.transition = "0.5s";
         discover_button.style.opacity = 1;
     }
 }, 100);
+
+const zamanlayici1 = setInterval(() => {
+    if (j < yazi1.length) {
+        if (text1) text1.textContent += yazi1[j];
+        j++;
+    } else {
+        clearInterval(zamanlayici1);
+        discover_button.style.transition = "0.5s";
+        discover_button.style.opacity = 1;
+    }
+}, 100);
+
+const zamanlayici2 = setInterval(() => {
+    if (k < yazi2.length) {
+        if (text2) text2.textContent += yazi2[k];
+        k++;
+    } else {
+        clearInterval(zamanlayici2);
+        discover_button.style.transition = "0.5s";
+        discover_button.style.opacity = 1;
+    }
+}, 100);
+
+if (discover_button) {
+    discover_button.addEventListener("click", function(){
+        window.location.href = "/sites/softwares.html";
+    });
+}
